34f001ef9cd5a93147810ca19861d1138c692f35
Andrew James <andrewpjames87@gmail.com>
2023-06-02 15:39:27 -0600

    feat: update rawrx page layout with photos

--- src/routes/rawrx.tsx
+++ src/routes/rawrx.tsx
@@ -1,9 +1,8 @@
-import { Box, Center, Image, SimpleGrid, VStack } from "@chakra-ui/react";
+import { Box, Center, Image, SimpleGrid, VStack, Text } from "@chakra-ui/react";
 import { AnimatedPageWrapper } from "../components/animated-page-wrapper";
 import { motion } from "framer-motion";
 import { ProjectOverview } from "../components/project-overview";
 import { Container } from "../components/container";
-import { Button } from "../components/button";
 import { projects } from "../App";
 
 const MotionBox = motion(Box);
@@ -19,45 +18,69 @@ export const RawRx = () => (
         skills={projects.find((p) => p.to === "/rawrx")?.skills ?? []}
         description="I collaborated with RawRx, a growing THC and CBD pharmaceutical company, to develop a brand that embodies their commitment to producing the cleanest and purest products in their industry. Below are key highlights of the brand creation process we underwent together."
       />
-      <Center bgColor="#a29160" h={["50vh", null, "80vh"]}>
-        <Image src="/images/tbr/tbr-hero.webp" />
+      <Center
+        bgImage="/images/rawrx/raw-background.webp"
+        backgroundSize="cover"
+        backgroundPosition="center"
+        h={["50vh", null, "80vh"]}
+      >
+        <Image src="/images/rawrx/raw-logo.webp" />
       </Center>
+      <Container pt={6} px={[2, null, 6]} pb={[3, null, 0]}>
+        <Text
+          fontSize={["4xl", "4xl"]}
+          fontWeight="900"
+          letterSpacing="-0.8px"
+          lineHeight={["2.25rem", "5rem"]}
+        >
+          LOGO PROPOSAL
+        </Text>
+      </Container>
       <Container py={12} px={[6]}>
         <VStack flexDir="column" spacing={12}>
           <SimpleGrid columns={[2]} spacing={12}>
-            <Image src="/images/tbr/first-drum-head.webp" />
-            <Image src="/images/tbr/second-drum-head.webp" />
-          </SimpleGrid>
-          <SimpleGrid columns={[4]} spacing={12}>
-            <Image src="/images/tbr/WCS-Patch-color.webp" />
-            <Image src="/images/tbr/outtasightsquare.webp" />
-            <Image src="/images/tbr/motorcyclemaniasquarepatch.webp" />
-            <Image src="/images/tbr/Boogie-Patch.webp" />
+            <Image src="/images/rawrx/RawRx-logo-proposal_Page_2.jpg" />
+            <Image src="/images/rawrx/RawRx-logo-proposal_Page_3.jpg" />
+            <Image src="/images/rawrx/RawRx-logo-proposal_Page_4.jpg" />
+            <Image src="/images/rawrx/RawRx-logo-proposal_Page_5.webp" />
           </SimpleGrid>
         </VStack>
       </Container>
-      <Center bgColor="#a29160" py={24}>
-        <iframe
-          title="Motorcycle Mania"
-          width="100%"
-          height="100%"
-          src="https://www.youtube.com/embed/LCJNw-oDY9c"
-          allow="autoplay;"
-          style={{
-            height: "75vh",
-            paddingLeft: "25px",
-            paddingRight: "25px",
-            maxWidth: "1500px",
-          }}
-        />
-      </Center>
+      <Container pt={6} px={[2, null, 6]} pb={[3, null, 0]}>
+        <Text
+          fontSize={["4xl", "4xl"]}
+          fontWeight="900"
+          letterSpacing="-0.8px"
+          lineHeight={["2.25rem", "5rem"]}
+        >
+          LOGO ITERATION
+        </Text>
+      </Container>
       <Container py={12} px={[6]}>
         <VStack flexDir="column" spacing={12}>
           <SimpleGrid columns={[2]} spacing={12}>
-            <Image src="/images/tbr/WCS-T-Shirt.webp" />
-            <Image src="/images/tbr/Outta-Sight-T-Shirt-Black.webp" />
-            <Image src="/images/tbr/MM-T-Shirt.webp" />
-            <Image src="/images/tbr/Boogie-Man-T-Shirt.webp" />
+            <Image src="/images/rawrx/RawRx-logo-r2_Page_2.webp" />
+            <Image src="/images/rawrx/RawRx-logo-r2_Page_3.webp" />
+          </SimpleGrid>
+        </VStack>
+      </Container>
+      <Container pt={6} px={[2, null, 6]} pb={[3, null, 0]}>
+        <Text
+          fontSize={["4xl", "4xl"]}
+          fontWeight="900"
+          letterSpacing="-0.8px"
+          lineHeight={["2.25rem", "5rem"]}
+        >
+          BUSINESS CARD DESIGN
+        </Text>
+      </Container>
+      <Container py={12} px={[6]}>
+        <VStack flexDir="column" spacing={12}>
+          <SimpleGrid columns={[2]} spacing={12}>
+            <Image src="/images/rawrx/rawFlattenedProposal_Page_2.webp" />
+            <Image src="/images/rawrx/rawFlattenedProposal_Page_3.webp" />
+            <Image src="/images/rawrx/rawFlattenedProposal_Page_4.webp" />
+            <Image src="/images/rawrx/rawFlattenedProposal_Page_1.webp" />
           </SimpleGrid>
         </VStack>
       </Container>
