4838f01aafb648fcebe52afbfdee26a2bacfff8f
Andrew James <andrewpjames87@gmail.com>
2023-06-06 17:25:03 -0600

    feat: mobile has video controls

--- src/routes/gamer.tsx
+++ src/routes/gamer.tsx
@@ -1,4 +1,11 @@
-import { Box, Center, Image, SimpleGrid, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  Image,
+  SimpleGrid,
+  VStack,
+  useMediaQuery,
+} from "@chakra-ui/react";
 import { AnimatedPageWrapper } from "../components/animated-page-wrapper";
 import { motion } from "framer-motion";
 import { ProjectOverview } from "../components/project-overview";
@@ -8,55 +15,60 @@ import { projects } from "../App";
 
 const MotionBox = motion(Box);
 
-export const Gamer = () => (
-  <AnimatedPageWrapper>
-    <MotionBox
-      // layoutId="gamer"
-      w="100%"
-    >
-      <ProjectOverview
-        href="https://gamer.xyz"
-        linkText="gamer.xyz"
-        title="GAMER.XYZ"
-        skills={projects.find((p) => p.to === "/gamer")?.skills ?? []}
-        description="I rebranded Arena to emphasize games and creators building engaged communities. This involved creating a new logo, updating the color palette, and redesigning the entire UI to reflect the new brand. Additionally, I implemented new features that strategically directed the product towards fostering community engagement."
-      />
-      <Center
-        bgImage="/images/gamer/gamer-bg.webp"
-        backgroundSize="cover"
-        backgroundPosition="center"
-        py={24}
+export const Gamer = () => {
+  const [isMobile] = useMediaQuery("(max-width: 768px)");
+
+  return (
+    <AnimatedPageWrapper>
+      <MotionBox
+        // layoutId="gamer"
+        w="100%"
       >
-        {/* <Image src="/images/gamer/gamer-hero.webp" /> */}
-        <video
-          // poster="/images/gamer/gamer-hero.webp"
-          src="/video/gamer-preview.mp4"
-          autoPlay
-          loop
-          style={{
-            width: "80vw",
-            maxWidth: "1500px",
-            filter: "drop-shadow(5px 5px 10px #000)",
-            borderRadius: "10px",
-          }}
+        <ProjectOverview
+          href="https://gamer.xyz"
+          linkText="gamer.xyz"
+          title="GAMER.XYZ"
+          skills={projects.find((p) => p.to === "/gamer")?.skills ?? []}
+          description="I rebranded Arena to emphasize games and creators building engaged communities. This involved creating a new logo, updating the color palette, and redesigning the entire UI to reflect the new brand. Additionally, I implemented new features that strategically directed the product towards fostering community engagement."
         />
-      </Center>
-      <Container py={12} px={[6]}>
-        <VStack flexDir="column" spacing={12}>
-          <SimpleGrid columns={[1, 2]}>
-            <Image src="/images/gamer/gamer-logos.webp" />
-            <Image src="/images/gamer/gamer-font-and-color.webp" />
-          </SimpleGrid>
-          <Image src="/images/gamer/gamer-dark.webp" w="full" />
-          <Image src="/images/gamer/gamer-light.webp" w="full" />
-          {/* <Image src="/images/gamer/gamer-pages.webp" w="full" /> */}
-        </VStack>
-      </Container>
-      <Center py={12}>
-        <Button onClick={() => window.open("https://gamer.xyz")}>
-          Checkout Gamer.xyz
-        </Button>
-      </Center>
-    </MotionBox>
-  </AnimatedPageWrapper>
-);
+        <Center
+          bgImage="/images/gamer/gamer-bg.webp"
+          backgroundSize="cover"
+          backgroundPosition="center"
+          py={24}
+        >
+          {/* <Image src="/images/gamer/gamer-hero.webp" /> */}
+          <video
+            // poster="/images/gamer/gamer-hero.webp"
+            controls={isMobile}
+            src="/video/gamer-preview.mp4"
+            autoPlay
+            loop
+            style={{
+              width: "80vw",
+              maxWidth: "1500px",
+              filter: "drop-shadow(5px 5px 10px #000)",
+              borderRadius: "10px",
+            }}
+          />
+        </Center>
+        <Container py={12} px={[6]}>
+          <VStack flexDir="column" spacing={12}>
+            <SimpleGrid columns={[1, 2]}>
+              <Image src="/images/gamer/gamer-logos.webp" />
+              <Image src="/images/gamer/gamer-font-and-color.webp" />
+            </SimpleGrid>
+            <Image src="/images/gamer/gamer-dark.webp" w="full" />
+            <Image src="/images/gamer/gamer-light.webp" w="full" />
+            {/* <Image src="/images/gamer/gamer-pages.webp" w="full" /> */}
+          </VStack>
+        </Container>
+        <Center py={12}>
+          <Button onClick={() => window.open("https://gamer.xyz")}>
+            Checkout Gamer.xyz
+          </Button>
+        </Center>
+      </MotionBox>
+    </AnimatedPageWrapper>
+  );
+};
--- src/routes/genisis.tsx
+++ src/routes/genisis.tsx
@@ -1,4 +1,4 @@
-import { Box, Center } from "@chakra-ui/react";
+import { Box, Center, useMediaQuery } from "@chakra-ui/react";
 import { AnimatedPageWrapper } from "../components/animated-page-wrapper";
 import { motion } from "framer-motion";
 import { ProjectOverview } from "../components/project-overview";
@@ -7,40 +7,45 @@ import { projects } from "../App";
 
 const MotionBox = motion(Box);
 
-export const Genesis = () => (
-  <AnimatedPageWrapper>
-    <MotionBox w="100%">
-      <ProjectOverview
-        href="https://mint.arena.gl"
-        linkText="mint.arena.gl"
-        title="GENESIS"
-        skills={projects.find((p) => p.to === "/genesis")?.skills ?? []}
-        description="This project was to create an immersive web experience fot the Genesis NFT project that would captivate users the same way that playing a video game would."
-      />
-      <Center
-        bgImage="/images/genesis/genesis-bg.webp"
-        backgroundSize="cover"
-        backgroundPosition="center"
-        py={24}
-      >
-        {/* <Image src="/images/genesis-hero.png" /> */}
-        <video
-          src="/video/genisis-preview.mp4"
-          autoPlay
-          loop
-          style={{
-            width: "80vw",
-            maxWidth: "1500px",
-            filter: "drop-shadow(5px 5px 10px #000)",
-            borderRadius: "10px",
-          }}
+export const Genesis = () => {
+  const [isMobile] = useMediaQuery("(max-width: 768px)");
+
+  return (
+    <AnimatedPageWrapper>
+      <MotionBox w="100%">
+        <ProjectOverview
+          href="https://mint.arena.gl"
+          linkText="mint.arena.gl"
+          title="GENESIS"
+          skills={projects.find((p) => p.to === "/genesis")?.skills ?? []}
+          description="This project was to create an immersive web experience fot the Genesis NFT project that would captivate users the same way that playing a video game would."
         />
-      </Center>
-      <Center py={12}>
-        <Button onClick={() => window.open("https://mint.arena.gl")}>
-          Checkout GENESIS
-        </Button>
-      </Center>
-    </MotionBox>
-  </AnimatedPageWrapper>
-);
+        <Center
+          bgImage="/images/genesis/genesis-bg.webp"
+          backgroundSize="cover"
+          backgroundPosition="center"
+          py={24}
+        >
+          {/* <Image src="/images/genesis-hero.png" /> */}
+          <video
+            controls={isMobile}
+            src="/video/genisis-preview.mp4"
+            autoPlay
+            loop
+            style={{
+              width: "80vw",
+              maxWidth: "1500px",
+              filter: "drop-shadow(5px 5px 10px #000)",
+              borderRadius: "10px",
+            }}
+          />
+        </Center>
+        <Center py={12}>
+          <Button onClick={() => window.open("https://mint.arena.gl")}>
+            Checkout GENESIS
+          </Button>
+        </Center>
+      </MotionBox>
+    </AnimatedPageWrapper>
+  );
+};
